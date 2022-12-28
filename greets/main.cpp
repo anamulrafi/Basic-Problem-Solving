b0d8612922aed513dd29e55f0418d5c1e2fd995d
AnamulRafi <anamulhoquerafi118399@gmail.com>
2022-12-28 12:27:23 +0600

    Write a program that asks the user for their name and greets them with their name.

---
+++ greets/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+//Write a program that asks the user for their name and greets them with their name.
+
+
+int main()
+{
+    string name;
+    cout << "What's your name?"  << endl;
+    cin >> name ;
+    cout<<" Hello " << name ;
+
+    return 0;
+}
